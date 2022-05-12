05455f259736773c93a12c568e9b75519eb39256
Pedrohvc1 <pedro.vieira02@hotmail.com>
2022-05-12 00:20:01 -0300

    primeiro ajuste tela cad_usuario

--- src/pages/Cad_usuario/index.js
+++ src/pages/Cad_usuario/index.js
@@ -7,18 +7,18 @@ import {useNavigation} from '@react-navigation/native'
 export default function Cad_usuario() {
   const navigation = useNavigation(); 
   return (
-    <View style={styles.container2}>
+    <View style={styles.container}>
      
      
-      <View style={styles.containerTitle2}>
-      <Text style={styles.title2}>EFICIÊNCIA ENERGÉTICA</Text>
+      <View style={styles.containerTitle}>
+      <Text style={styles.title}>EFICIÊNCIA ENERGÉTICA</Text>
        
       </View>
 
       <View
         style={styles.containerForm}>
             
-            <Text>CRIAR CONTA</Text>  
+            <Text style={styles.title2}>CRIAR CONTA</Text>  
 
             <TextInput
             style={styles.input}
@@ -46,7 +46,7 @@ export default function Cad_usuario() {
             onChangeText={()=>{}}
             />
                 
-                <Text>Ao criar uma conta, você concorda com os 
+                <Text style={styles.x}>Ao criar uma conta, você concorda com os 
                   Termos de Uso e Privacidade do aplicativo</Text>  
             <TouchableOpacity 
             style ={styles.btnContinue}>
@@ -65,6 +65,66 @@ export default function Cad_usuario() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor: '#ffff'        
-  }
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',        
+    width: '90%',        
+  },
+    title:{       
+      fontSize: 20,
+      fontWeight:'bold',
+      marginTop: 28,
+      marginBottom: 12,
+      color:'#004c94',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    title2:{
+      fontSize: 20,
+      marginTop: 28,
+      marginBottom: 12,
+      color:'#004c94',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    containerForm:{
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center',        
+      width: '90%',
+  },
+  input:{
+      backgroundColor: '#ffff',
+      borderWidth: 2,
+      borderColor: '#004c94',
+      width: '30%',
+      marginBottom:15,
+      color: '#004c94', //azul
+      fontSize: 17,     
+      padding: 10,
+      alignItems: 'center',
+      justifyContent:'center'       
+    },
+    btnContinue:{
+      backgroundColor: '#004c94',
+      borderRadius: 50,
+      paddingVertical: 8,
+      width: '30%',
+      alignSelf: 'short',
+      bottom: 'null',
+      alignItems: 'center',
+      justifyContent:'center',        
+      height: 50,
+
+    },
+    continueText:{
+        fontSize: 12,
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    x:{
+      fontSize: 14,
+      color: '#004c94',
+  },
+         
 })
